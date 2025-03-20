3e5bfdebd39052d99d9847fa7e403b74fbbc4311
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-20 17:11:43 +0530

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Block from "./component/Block";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(Array(9).fill(null));
+  const [currentTurn, setCurrentTurn] = useState("X");
 
+  // console.log("State", state)
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < win.length; i++) {
+      console.log(win.length);
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c])
+        return true;
+    }
+    return false;
+  };
+  const handleBlockClick = (index: number) => {
+    const stateCopy = Array.from(state);
+    stateCopy[index] = currentTurn;
+    if (stateCopy[index] === null) return;
+
+    const win = checkWinner(stateCopy);
+    if (win) {
+      alert(`${currentTurn} won the game`);
+    }
+    setCurrentTurn(currentTurn === "X" ? "O" : "X");
+    setState(stateCopy);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="board">
+      <div className="row">
+        <Block onClick={() => handleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => handleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => handleBlockClick(2)} value={state[2]} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="row">
+        <Block onClick={() => handleBlockClick(3)} value={state[3]} />
+        <Block onClick={() => handleBlockClick(4)} value={state[4]} />
+        <Block onClick={() => handleBlockClick(5)} value={state[5]} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="row">
+        <Block onClick={() => handleBlockClick(6)} value={state[6]} />
+        <Block onClick={() => handleBlockClick(7)} value={state[7]} />
+        <Block onClick={() => handleBlockClick(8)} value={state[8]} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
